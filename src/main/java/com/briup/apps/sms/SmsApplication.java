dc21bb4960fe7759c705bb46e5fdd3f1c30088c0
修改 <501421748@qq.com>
2019-07-09 10:58:11 +0800

    [韩京韦]修改控制信息

--- src/main/java/com/briup/apps/sms/SmsApplication.java
+++ src/main/java/com/briup/apps/sms/SmsApplication.java
@@ -2,7 +2,11 @@ package com.briup.apps.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 用户控制
+ * @author 韩京韦
+ *
+ */
 @SpringBootApplication
 public class SmsApplication {
 
