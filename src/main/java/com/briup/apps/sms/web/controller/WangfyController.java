916ddd4719424b9bdf83575743ccb42639e6d14a
王富源 <1975813983@qq.com>
2019-07-09 11:30:28 +0800

    [王富源]改写注释

--- src/main/java/com/briup/apps/sms/web/controller/WangfyController.java
+++ src/main/java/com/briup/apps/sms/web/controller/WangfyController.java
@@ -1,10 +1,11 @@
 package com.briup.apps.sms.web.controller;
-
+/**
+ * 王富源的类
+ * @author Wangfy
+ *
+ */
 public class WangfyController {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
