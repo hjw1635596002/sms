954f7ec06ebbbb312b170a571ece3bec87041ac4
梁永红 <2826791190@qq.com>
2019-07-09 14:30:51 +0800

    [梁永红]给主窗口创建的类添加注释

--- src/main/java/com/briup/apps/sms/web/controller/LiangyhController.java
+++ src/main/java/com/briup/apps/sms/web/controller/LiangyhController.java
@@ -1,16 +1,23 @@
 package com.briup.apps.sms.web.controller;
 /**
- * 我的类
+ * 主窗口创建的类
+ * a的值是一个私有变量
  * @author lenovo
- *
+ * @
  */
 public class LiangyhController {
 	private int a=32;
-
+	/**
+	 * 获取成员变量a的值
+	 * @return
+	 */
 	public int getA() {
 		return a;
 	}
-
+    /**
+     * 设置参数a的值
+     * @param a
+     */
 	public void setA(int a) {
 		this.a = a;
 	}
