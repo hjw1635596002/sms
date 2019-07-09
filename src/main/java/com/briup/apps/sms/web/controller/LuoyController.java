ff02d200185dd745e0d734f6c805cd05e1e312d6
罗云 <823753287@qq.com>
2019-07-09 17:17:20 +0800

    [罗云]更新了LuoyController类

--- src/main/java/com/briup/apps/sms/web/controller/LuoyController.java
+++ src/main/java/com/briup/apps/sms/web/controller/LuoyController.java
@@ -3,5 +3,34 @@ package com.briup.apps.sms.web.controller;
  * 罗云修改的类*/
 
 public class LuoyController {
-
+	public class Course {
+	    private Long id;
+	    private String name;
+	    private String credit;
+	    private Long user_id;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getCredit() {
+			return credit;
+		}
+		public void setCredit(String credit) {
+			this.credit = credit;
+		}
+		public Long getUser_id() {
+			return user_id;
+		}
+		public void setUser_id(Long user_id) {
+			this.user_id = user_id;
+		} 
+}
 }
