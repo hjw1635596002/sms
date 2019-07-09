39871bb7519297d75c7ec0cbc56fe061474e3f58
罗云 <823753287@qq.com>
2019-07-09 17:29:53 +0800

    [罗云]修改自己的类

--- src/main/java/com/briup/apps/sms/web/controller/LuoyController.java
+++ src/main/java/com/briup/apps/sms/web/controller/LuoyController.java
@@ -1,7 +1,7 @@
 package com.briup.apps.sms.web.controller;
 
 /**
- * 
+ * 罗云的类
  * @author LENOVO
  *
  */
@@ -30,6 +30,7 @@ public class LuoyController {
 			this.credit = credit;
 		}
 		public Long getUser_id() {
+			
 			return user_id;
 		}
 		public void setUser_id(Long user_id) {
