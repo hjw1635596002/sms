3d6a74b1ed2f6e7638e0bf89682d62588170e1c3
韩京韦 <501421748@qq.com>
2019-07-09 14:30:42 +0800

    [韩京韦]调用LiangyhController类输出变量a

--- src/main/java/com/briup/apps/sms/SmsApplication.java
+++ src/main/java/com/briup/apps/sms/SmsApplication.java
@@ -2,6 +2,8 @@ package com.briup.apps.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.briup.apps.sms.web.controller.HanjwController;
 /**
  * 用户控制
  * @author 韩京韦
@@ -12,6 +14,8 @@ public class SmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsApplication.class, args);
+		HanjwController hjw = new HanjwController();
+		
 	}
 
 }
--- src/main/java/com/briup/apps/sms/web/controller/LiangyhController.java
+++ src/main/java/com/briup/apps/sms/web/controller/LiangyhController.java
@@ -6,11 +6,21 @@ package com.briup.apps.sms.web.controller;
  */
 public class LiangyhController {
 	private int a=32;
-
+	/**
+	 * 当前年份
+	 */
+	final static int YEAR=2019;
+	/**
+	 * 获取成员变量a的值
+	 * @return
+	 */
 	public int getA() {
 		return a;
 	}
-
+	/**
+	 * 设置变量a
+	 * @param a
+	 */
 	public void setA(int a) {
 		this.a = a;
 	}
