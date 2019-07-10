7a0be63069dcde6e3bdb2ad4bebfd98a3b8b3a7e
刘盛明 <1261709288@qq.com>
2019-07-10 13:00:19 +0800

    [刘盛明]类修改

---
+++ src/main/java/com/briup/apps/sms/dao/CollegeDao.java
@@ -0,0 +1,18 @@
+package com.briup.apps.sms.dao;
+
+import java.util.List;
+
+import com.briup.apps.sms.bean.College;
+
+
+public interface CollegeDao {
+	//查询所有
+	List<College> selectAll();
+	
+	//插入
+	void insert(College college);
+	
+	//修改
+	void update(College college);
+	
+}
