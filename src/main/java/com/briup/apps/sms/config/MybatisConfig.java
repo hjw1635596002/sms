a106ccf5b39cfb0dbe002619d908b3f66947f8c5
韩京韦 <501421748@qq.com>
2019-07-10 10:37:54 +0800

    [韩京韦]更新文件

--- src/main/java/com/briup/apps/sms/config/MybatisConfig.java
+++ src/main/java/com/briup/apps/sms/config/MybatisConfig.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.briup.apps.sms.dao")//去哪里找接口
+@MapperScan("com.briup.apps.sms.dao")//去哪里找接口 
 public class MybatisConfig {
 
 }
